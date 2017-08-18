a00a6fe93c9f5b850968583233ce4ef1aa3a22cd
AmarSiddi <31123423+AmarSiddi@users.noreply.github.com>
2017-08-18 13:48:01 +0530

    Update LongestWord.java

--- LongestWord.java
+++ LongestWord.java
@@ -7,9 +7,8 @@ public class LongestWord {
 
 	public static void main(String[] args)
 	{
-		String sen="amar## 343siddi  pavankumar";
-		//System.out.println(sen.replaceAll("\\sen+",""));
-
+		String sen="java## 343Testing  Program";
+		
 		Pattern pattern = Pattern.compile("[^a-z A-Z]");
 		Matcher matcher = pattern.matcher(sen);
 		String str1 = matcher.replaceAll("");
@@ -25,20 +24,26 @@ public class LongestWord {
 			}
 		}
 		System.out.println(longest);
+		
+		
+		
+		//This is to count each worl length in a string
+		/*
 		int count=0;
 		
 		for(int i=0; i<str1.length();i++)
 		{
 			if(str1.charAt(i)==' ')
 			{
-				System.out.println("count is " + count);
+				System.out.println("count of a word is" + count);
 	            count = 0;
 	        } else {
 	            count++;
 	        }
 		}
 		System.out.println(count);
-		//return number;
+		return number;
+		*/
 
 	}
 
