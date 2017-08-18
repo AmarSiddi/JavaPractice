250836921423928eae05e114b7098a5283c4ac32
AmarSiddi <31123423+AmarSiddi@users.noreply.github.com>
2017-08-18 13:03:20 +0530

    Add files via upload

---
+++ LongestWord.java
@@ -0,0 +1,46 @@
+package interview;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LongestWord {
+
+	public static void main(String[] args)
+	{
+		String sen="amar## 343siddi  pavankumar";
+		//System.out.println(sen.replaceAll("\\sen+",""));
+
+		Pattern pattern = Pattern.compile("[^a-z A-Z]");
+		Matcher matcher = pattern.matcher(sen);
+		String str1 = matcher.replaceAll("");
+		
+		
+		String longest = "";
+		
+		String[] words = str1.split(" ");
+		System.out.println(words);
+		for(String word:words){
+			if(word.length()>longest.length()){
+				longest = word;
+			}
+		}
+		System.out.println(longest);
+		int count=0;
+		
+		for(int i=0; i<str1.length();i++)
+		{
+			if(str1.charAt(i)==' ')
+			{
+				System.out.println("count is " + count);
+	            count = 0;
+	        } else {
+	            count++;
+	        }
+		}
+		System.out.println(count);
+		//return number;
+
+	}
+
+
+}
